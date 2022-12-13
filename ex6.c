16928d60b0fde641da9f76f2bea13cab1d167375
Nicolas Freitas Silveira <97970315+NfSilveira@users.noreply.github.com>
2022-12-13 15:04:05 -0300

    Projeto Exercício 6 - Adega de Vinhos

--- ex6.c
+++ ex6.c
@@ -1,74 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
 
 typedef struct {
+
   char wine_name[50];
   char wine_country[50];
   char wine_grape_kind[50];
-  int wine_harvest_year;
+  char wine_harvest_year[50];
+
 } Wine;
 
+
 typedef struct Node {
+
   Wine *wine;
   struct Node *next;
   struct Node *prev;
+
 } Node;
+
 
 typedef struct {
 
   Node *head;
-
   Node *tail;
-
   int amount;
+
 } Queue;
 
 
-Queue *initialize_queue() {
+Queue *init_queue() {
 
-  Queue *q = malloc(sizeof(Queue));
+  Queue *q;
 
-  q->head = NULL;
-  q->tail = NULL;
-  q->amount = 0;
+  q = (Queue *) calloc(1, sizeof(*q));
+
+  q -> head = q -> tail = NULL;
+  q -> amount = 0;
 
   return q;
 }
 
 
-void clear_inputs() {
+bool is_empty(Queue *q) {
 
-  fflush(stdin);
-  __fpurge(stdin);
-}
+	if (q -> amount == 0) {
+		return true;
+    }
 
-
-int is_empty(Queue *q) {
-
-  return q->amount == 0;
+	return false;
 }
 
 
 Wine *create_new_wine() {
 
-  Wine *wine = malloc(sizeof(Wine));
+  Wine *wine = (Wine *) calloc(1, sizeof(*wine));
 
-  clear_inputs();
-  printf("Digite o nome do vinho: \n");
-  fgets(wine->wine_name, 50, stdin);
+  printf("Vamos começar! Qual é o nome do vinho a ser adicionado? \n");
+  scanf(" %[^\n]", wine -> wine_name);
+  getchar();
 
-  clear_inputs();
-  printf("Digite o país de origem do vinho: \n");
-  fgets(wine->wine_country, 50, stdin);
+  printf("Agora, em que país ele foi produzido? \n");
+  scanf(" %[^\n]", wine -> wine_country);
+  getchar();
 
-  clear_inputs();
-  printf("Digite o tipo da uva do vinho: \n");
-  fgets(wine->wine_grape_kind, 50, stdin);
+  printf("Entendi... Qual uva foi utilizada na sua produção? \n");
+  scanf(" %[^\n]", wine -> wine_grape_kind);
+  getchar();
 
-  clear_inputs();
-  printf("Digite o ano da safra do vinho: \n");
-  scanf("%d", &wine->wine_harvest_year);
+  printf("É a última pergunta, te juro! Qual é o ano da safra desse vinho? \n");
+  scanf(" %[^\n]", wine -> wine_harvest_year);
+  getchar();
 
   return wine;
 }
@@ -76,193 +81,142 @@ Wine *create_new_wine() {
 
 void push(Queue *q, Wine *wine) {
 
-  Node *node = malloc(sizeof(Node));
-  node->wine = wine;
-  node->next = NULL;
+  Node *node = (Node *) calloc(1, sizeof(*node));
+  
+  node -> wine = wine;
+  node -> next = NULL;
 
   if (is_empty(q)) {
 
-    q->head = node;
-    node->prev = NULL;
+    q -> head = node;
+    node -> prev = NULL;
+  } else {
+    
+    node -> prev = q -> tail;
+    q -> tail -> next = node;
   }
-  else {
 
-    node->prev = q->tail;
-    q->tail->next = node;
-  }
-
-  q->tail = node;
-  q->amount++;
+  q -> tail = node;
+  q -> amount++;
 }
 
 
-void shift(Queue *q) {
+void i_like_to_move_it(Queue *q) {
 
   if (is_empty(q)) {
 
-    printf("A fila está vazia!\n");
+    printf("Parceiro(a), a fila tá vazia.\n");
     return;
   }
 
-  Node *aux = q->head;
+  Node *aux = q -> head;
 
-  if (q->head->next != NULL) {
+  if (q -> head -> next != NULL) {
 
-    q->head = q->head->next;
+    q -> head = q -> head -> next;
   }
   else {
 
-    q->head = NULL;
+    q -> head = NULL;
   }
 
-  q->amount--;
+  q -> amount--;
   free(aux);
 }
 
 
-void print_wine(Wine *wine) {
+void display_wine_information(Wine *wine) {
 
   printf("\n");
 
-  printf("Nome do vinho: %s\n", wine->wine_name);
-  printf("País de origem: %s\n", wine->wine_country);
-  printf("Tipo da uva: %s\n", wine->wine_grape_kind);
-  printf("Safra: %d\n", wine->wine_harvest_year);
+  printf("Nome: %s\n", wine -> wine_name);
+  printf("País: %s\n", wine -> wine_country);
+  printf("Tipo da uva: %s\n", wine -> wine_grape_kind);
+  printf("Safra: %s\n", wine -> wine_harvest_year);
 
   printf("\n");
 }
 
 
-void print_5_most_recent_records(Queue *wine_house) {
+void show_5_most_recent_additions(Queue *wine_house) {
 
   if (is_empty(wine_house)) {
 
-    printf("Não há vinhos adicionados à \"adega\"!\n");
+    printf("Amigão, não tem nenhum vinho na adega. Acho que você bebeu demais...\n");
     return;
   }
+  printf("\nOk, aqui estão os 5 vinhos mais recentes: \n");
 
-  printf("\n5 Vinhos mais recentes: \n");
-
-  Node *tail = wine_house->tail;
+  Node *tail = wine_house -> tail;
   int i = 0;
 
   while (tail && i < 5) {
-
-    print_wine(tail->wine);
-    tail = tail->prev;
+    display_wine_information(tail -> wine);
+    tail = tail -> prev;
     i++;
   }
 }
 
 
-void print_5_oldest_registered_wines(Queue *wine_house) {
+void show_5_oldest_additions(Queue *wine_house) {
 
   if (is_empty(wine_house)) {
 
-    printf("Não há vinhos adicionados à \"adega\"!\n");
+    printf("Amigão, não tem nenhum vinho na adega. Acho que você bebeu demais...\n");
     return;
   }
-  printf("\nVinhos mais antigos: \n");
+  printf("\nBeleza, aqui estão os 5 vinhos mais antigos: \n");
 
-  Node *head = wine_house->head;
+  Node *head = wine_house -> head;
   int i = 0;
 
   while (head && i < 5) {
 
-    print_wine(head->wine);
-    head = head->next;
+    display_wine_information(head -> wine);
+    head = head -> next;
     i++;
   }
 }
 
 
-void oldest_registered_wine(Queue *wine_house) {
+void oldest_wine(Queue *wine_house) {
 
   if (is_empty(wine_house)) {
 
-    printf("A \"adega\" está vazia!\n");
+    printf("Amigão, não tem nenhum vinho na adega. Acho que você bebeu demais...\n");
     return;
   }
 
-  print_wine(wine_house->head->wine);
-  shift(wine_house);
+  display_wine_information(wine_house -> head -> wine);
+  i_like_to_move_it(wine_house);
 }
 
 
-void recent_registered_wine(Queue *wine_house) {
+void recently_registered_wine(Queue *wine_house) {
 
   if (is_empty(wine_house)) {
 
-    printf("A \"adega\" está vazia!\n");
+    printf("Amigão, não tem nenhum vinho na adega. Acho que você bebeu demais...\n");
     return;
   }
 
-  print_wine(wine_house->tail->wine);
+  display_wine_information(wine_house -> tail -> wine);
 }
 
 
-void print_wine_queue(Queue *q) {
+void display_wine_information_queue(Queue *q) {
 
   if (is_empty(q)) {
 
-    printf("A \"adega\" está vazia!\n");
+    printf("A adega está vazia...\n");
     return;
   }
 
-  Node *head = q->head;
+  Node *head = q -> head;
 
   while (head) {
-    print_wine(head->wine);
-    head = head->next;
-  }
-}
-
-
-int asc_order_check(Queue *q) {
-
-  Node *head = q->head;
-
-  while (head) {
-
-    if (!head->next) {
-
-      break;
-    }
-
-    if (head->wine->wine_harvest_year > head->next->wine->wine_harvest_year) {
-
-      return 0;
-    }
-
-    head = head->next;
-  }
-
-  return 1;
-}
-
-
-void asc_order_queue(Queue *q) {
-
-  while (!asc_order_check(q)) {
-
-    Node *head = q->head;
-
-    while (head) {
-
-      if (!head->next) {
-
-        break;
-      }
-
-      if (head->wine->wine_harvest_year > head->next->wine->wine_harvest_year) {
-
-        Wine *aux = head->wine;
-        head->wine = head->next->wine;
-        head->next->wine = aux;
-      }
-
-      head = head->next;
-    }
+    display_wine_information(head -> wine);
+    head = head -> next;
   }
 }
 
@@ -271,75 +225,84 @@ void add_new_wine(Queue *wine_house) {
 
   Wine *wine = create_new_wine();
   push(wine_house, wine);
-  asc_order_queue(wine_house);
 }
 
 
 int main(void) {
 
-  Queue *wine_house = initialize_queue();
-  Queue *special_occasion_wines = initialize_queue();
+  Queue *wine_house = init_queue();
+  Queue *special_occasion_wines = init_queue();
 
-  while (1) {
+  char wine_name[50]; 
 
-    int choice;
-    printf("\n\"Sistema de Adega Virtual Ifense! Escolha a opção:\"\n");
-    printf("1 - Quero adicionar um vinho!\n");
+  int choice;
+
+  printf("\nBem-vindo à sua adega!\n");
+
+  while (choice != 0) {
+
+    printf("\n1 - Quero adicionar um vinho!\n");
     printf("2 - Quero ver todos os vinhos!\n");
-    printf("3 - Quero ver todos os vinhos na lista especial!\n");
-    printf("4 - Quero ver o vinho mais antigo (não especial)!\n");
-    printf("5 - Quero guardar o vinho mais recente (ocasião especial) em outra "
-           "estrutura\n");
-    printf("6 - Quero ver o vinho mais recente (ocasião especial)!\n");
-    printf("7 - Quero ver os 5 vinhos mais antigos!\n");
-    printf("8 - Quero ver os 5 vinhos mais recentes!\n");
-    printf("Insira qualquer outro caractere para sair.\n\n");
+    printf("3 - Quero ver e abrir o vinho mais antigo (não especial)!\n");
+    printf("4 - Quero disponibilizar o vinho mais recente (ocasião especial) em uma adega especial!\n");
+    printf("5 - Quero ver o vinho mais recente (ocasião especial)!\n");
+    printf("6 - Quero ver os 5 vinhos mais antigos!\n");
+    printf("7 - Quero ver os 5 vinhos mais recentes!\n");
+    printf("0 - Quero sair.\n\n");
+    printf("Escolha a opção: ");
     scanf("%d", &choice);
 
 
     switch(choice) {
       case 1:
 
+        printf("\n");
         add_new_wine(wine_house);
+        break;
 
       case 2:
 
-        print_wine_queue(wine_house);
+        printf("\n");
+        display_wine_information_queue(wine_house);
+        break;
 
       case 3:
 
-        print_wine_queue(special_occasion_wines);
+        printf("\n");
+        oldest_wine(wine_house);
+        break;
 
       case 4:
 
-        oldest_registered_wine(wine_house);
+        push(special_occasion_wines, wine_house->tail->wine);
+        printf("\nO vinho mais recente foi inserido em uma seleção especial! Você ainda pode vê-lo na lista geral(opção 2) e exibição das adegas finais(opção 0)!\n");
+        break;
 
       case 5:
 
-        push(special_occasion_wines, wine_house->tail->wine);
+        printf("\n");
+        recently_registered_wine(wine_house);
+        break;
 
       case 6:
 
-        recent_registered_wine(wine_house);
+        printf("\n");
+        show_5_oldest_additions(wine_house);
+        break;
 
       case 7:
 
-        print_5_oldest_registered_wines(wine_house);
-
-      case 8:
-
-        print_5_most_recent_records(wine_house);
-
-      default:
+        printf("\n");
+        show_5_most_recent_additions(wine_house);
         break;
     }
   }
 
-  printf("\n\nAdega de Vinhos: \n\n");
-  print_wine_queue(wine_house);
+  printf("\n\nAdega: \n\n");
+  display_wine_information_queue(wine_house);
 
-  printf("\n\nSeleção de Vinhos Especiais: \n\n");
-  print_wine_queue(special_occasion_wines);
+  printf("\n\nVinhos para Ocasiões Especiais: \n\n");
+  display_wine_information_queue(special_occasion_wines);
 
   return 0;
 }
